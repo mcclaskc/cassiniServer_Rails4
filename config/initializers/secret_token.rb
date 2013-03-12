67d6b5fd35e0cf360afa27c01d55c9f931c68e73
Chris McClaskey <christopher.m.mcclaskey@gmail.com>
2013-03-12 15:30:10 -0600

    create rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CassiniServerRails4::Application.config.secret_key_base = 'b6b01da89e231cf8091539819e75174cbd0e2459c1fa0b4fa4f51308def4a3822b6716a4d9b8bf3812ffd35884778f5e9fdd7540954264c650585310c72e421b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+CassiniServerRails4::Application.config.session_store :encrypted_cookie_store, key: '_cassiniServer_Rails4_session'
