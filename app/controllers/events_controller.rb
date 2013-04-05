042e2acecca8401db38b84feb2e2da396e2c5041
Chris McClaskey <christopher.m.mcclaskey@gmail.com>
2013-04-05 07:41:36 -0600

    abstracted importing process to Event

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -69,44 +69,10 @@ class EventsController < ApplicationController
       unless file  
         raise "File not found. Did you remember to choose a file to upload?"      
       end
-      csv = CSV.parse(file.read)
-      csv.each do |r|
-      #CSV.foreach('../Downloads/S17.tol.csv') do |r|
-        column = 0
-        looking_for_next = false
-        request = ""
-        start_scet = ""
-        end_scet = ""
-        r[0].each_char do |c|
-          if c == ' '
-            looking_for_next = true
-            next
-          end
-          if looking_for_next
-            column += 1
-            looking_for_next = false
-          end
-
-          case column
-          when 0
-            request << c
-          when 1
-            start_scet << c
-          when 2
-            end_scet << c
-          else
-            break
-          end
-        end
-        event = Event.new(request: request, start_scet: start_scet, end_scet: end_scet)
-        # p request
-        # p start_scet
-        # p end_scet
-        event.save!  
-      end
+      Event.parse_and_create(file)
     rescue => e
       raise e
-      #flash[:error] = e
+      #flash[:error]
     end
     redirect_to :back
   end
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,39 @@
 class Event < ActiveRecord::Base
+	
+	def self.parse_and_create(file)
+		csv = CSV.parse(file.read)
+    csv.each do |r|
+      column = 0
+      looking_for_next = false
+      request = ""
+      start_scet = ""
+      end_scet = ""
+      r[0].each_char do |c|
+        if c == ' '
+          looking_for_next = true
+          next
+        end
+        if looking_for_next
+          column += 1
+          looking_for_next = false
+        end
+
+        case column
+        when 0
+          request << c
+        when 1
+          start_scet << c
+        when 2
+          end_scet << c
+        else
+          break
+        end
+      end
+      event = Event.new(request: request, start_scet: start_scet, end_scet: end_scet)
+      # p request
+      # p start_scet
+      # p end_scet
+      event.save!  
+    end
+	end
 end
