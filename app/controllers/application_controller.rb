6e4841c947de2a49da344a43581675da18600186
Chris McClaskey <christopher.m.mcclaskey@gmail.com>
2013-04-10 19:56:03 -0600

    added exception printing to log

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,10 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  rescue_from Exception, with: lambda { |exception| render_error 500, exception }
+
+  def render_error(status, e)
+   p e
+  end
 end
