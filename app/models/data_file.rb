030b84bef4591ceaf65992d0ef5018ae5f7fd7fa
Chris McClaskey <christopher.m.mcclaskey@gmail.com>
2013-04-13 16:23:36 -0600

    basic file importing

--- app/models/data_file.rb
+++ app/models/data_file.rb
@@ -1,6 +1,25 @@
 class DataFile < ActiveRecord::Base
 
 	def self.import_and_create(data_file)
-		#file name parsing and saving
+		#data_file is expected to be of type ActionDispatch::Http::UploadedFile
+
+		title = data_file.original_filename
+		
+		#uvisFOV_titan_2009-JUN-23.dat
+		string_date = ""
+		under_count = 0
+		title.each_char do |c|
+			if under_count == 2
+				break if c == '.'
+				string_date << c
+				next
+			elsif c == '_'
+				under_count += 1
+			end
+		end
+		
+		file_date = Date.parse(string_date)
+
+		DataFile.new(path: title, file_date: file_date).save!
 	end
 end
