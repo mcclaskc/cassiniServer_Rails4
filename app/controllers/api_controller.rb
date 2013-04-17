a436ae67c4b0afc1a55a7e3bb6f57d5bb1e7a581
Chris McClaskey <christopher.m.mcclaskey@gmail.com>
2013-04-17 15:34:01 -0600

    properly parsing param[date] string and returning correct records

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,37 +1,46 @@
 class ApiController < ApplicationController
 	def uvis
-
+		prepare_response
+		respond
 	end
 
 	def ephem
-		begin
-			@response[:content] = Ephem.where(time_stamp: @datetime)
-		rescue => e
-			@response[:status] = 500
-			@respinse[:details] = e
+		prepare_response
+		if @response[:status] == 200
+			begin
+				@response[:content] = Ephem.where(timestamp: @datetime)
+			rescue => e
+				@response[:status] = 500
+				@response[:details] = e.message
+			end
 		end
+		respond
 	end
 
 	def events
-
+		prepare_response
+		respond
 	end
 
 	private
 
+	#datetime and end_datetime need to look like "2009-06-22 12:00:00"
+
 	def prepare_response
 		@response = {status: 200, details: "OK", content: {}}
-		if @datetime = params[:datetime]
+		if @datetime = params['datetime']
+			@end_datetime = params['end_datetime']
 			begin 
-				@datetime = DateTime.parse(datetime)
-			rescue
+				@datetime = Time.zone.parse(@datetime)
+				@end_datetime = Time.zone.parse(@end_datetime) if @end_datetime
+			rescue => e
 				@response[:status] = 402
 				@response[:details] = "Invalid datetime"
 			end
 		else
 			@response[:status] = 401
-			@response[:details] = "Please Include a datetime or datetime range"
+			@response[:details] = "Please Include a datetime"
 		end
-		respond unless @response[:status] == 200
 	end
 
 	def respond
