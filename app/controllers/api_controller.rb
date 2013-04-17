dbb5f4b67e2e4c31c0223f5a8cc80fbb0c5254f2
Chris McClaskey <christopher.m.mcclaskey@gmail.com>
2013-04-17 16:15:42 -0600

    added check_date and abstracted check_datetime out of other api methods

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,11 +1,17 @@
 class ApiController < ApplicationController
 	def uvis
 		prepare_response
+		check_file_type
+		check_date
+			if @response[:status] = 200
+				
+			end	
 		respond
 	end
 
 	def ephem
 		prepare_response
+		check_datetime
 		if @response[:status] == 200
 			begin
 				@response[:content] = Ephem.where(timestamp: @datetime)
@@ -18,6 +24,7 @@ class ApiController < ApplicationController
 	end
 
 	def events
+		check_datetime
 		prepare_response
 		@response[:status] = 404
 		@response[:details] = "Invalid Request"
@@ -30,18 +37,53 @@ class ApiController < ApplicationController
 
 	def prepare_response
 		@response = {status: 200, details: "OK", content: {}}
+	end
+
+	def check_datetime
 		if @datetime = params['datetime']
 			@end_datetime = params['end_datetime']
 			begin 
 				@datetime = Time.zone.parse(@datetime)
 				@end_datetime = Time.zone.parse(@end_datetime) if @end_datetime
 			rescue => e
-				@response[:status] = 402
+				@response[:status] = 502
 				@response[:details] = "Invalid datetime"
 			end
 		else
-			@response[:status] = 401
+			@response[:status] = 501
 			@response[:details] = "Please Include a datetime"
+		end
+	end
+
+	def check_date
+		if @date = params['date']
+			@end_date = params['end_date']
+			begin
+			 	@date = Date.parse(@date)
+			 	@end_date = Date.parse(@date_time)
+			rescue => e
+			 	@response[:status] = 502
+				@response[:details] = "Invalid datetime"
+			end 
+		else
+			@response[:status] = 501
+			@response[:details] = "Please Include a date"
+		end
+	end
+
+	def get_file_type_id
+		if file_type = params['file_type']
+			file_type_id = nil
+			FileType.pluck(:id, :title).each do |ft|
+				file_type_id = ft[0] if file_type.downcase.include? ft[1].downcase
+			end
+			if file_type_id.nil?
+				@response[:status] = 502
+				@response[:details] = "Invalid File Type"
+			end
+		else
+			@response[:status] = 501
+			@response[:details] = "Please Include a file_type"
 		end
 	end
 
