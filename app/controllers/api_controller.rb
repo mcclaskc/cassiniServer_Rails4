1f9ceabed74971dac26b53ef9342e38e80e3fb2e
Chris McClaskey <christopher.m.mcclaskey@gmail.com>
2013-04-30 16:36:52 -0600

    documentation for apicontroller

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,4 +1,12 @@
+##
+# Controls the Web API directory 
+# For documentation on how to use the API from the client, go here: TODO LINK
 class ApiController < ApplicationController
+
+	##
+	# Handles requests for files.  
+	# Requires params['date'], params['file_type'] 
+	# Optional params['end_date']
 	def files
 		prepare_response
 		check_file_type
@@ -9,6 +17,10 @@ class ApiController < ApplicationController
 		respond
 	end
 
+	##
+	# Handles requests for files.  
+	# Requires params['date'], params['file_type'] 
+	# Optional params['end_date']
 	def ephem
 		prepare_response
 		check_datetime
@@ -40,6 +52,9 @@ class ApiController < ApplicationController
 		respond
 	end
 
+	##
+	# Handles requests for event data.  
+	# *Not Yet Implemented, sets response to 404
 	def events
 		check_datetime
 		prepare_response
@@ -50,12 +65,17 @@ class ApiController < ApplicationController
 
 	private
 
-	#datetime and end_datetime need to look like "2009-06-22 12:00:00"
-
+	## 
+	# Initializes instance variable @response (hash).
+	# All API methods use this.
 	def prepare_response
 		@response = {status: 200, details: "OK", content: {}}
 	end
 
+	##
+	# Ensures the presence and structure of datetime, then sets it to @datetime.
+	# Also checks for end_dateime, and sets it to @end_datetime if found.
+	# API methods that require datetime parameters should call this.
 	def check_datetime
 		if @datetime = params['datetime']
 			@end_datetime = params['end_datetime']
@@ -72,6 +92,10 @@ class ApiController < ApplicationController
 		end
 	end
 
+	##
+	# Ensures the presence and structure of date, then sets it to @date.
+	# Also checks for end_date, and sets it to @end_datet if found.
+	# API methods that require date parameters should call this.
 	def check_date
 		if @date = params['date']
 			@end_date = params['end_date']
@@ -88,6 +112,10 @@ class ApiController < ApplicationController
 		end
 	end
 
+	##
+	# Ensures the presence of validity of file_type, then sets it's 
+	# corresponding id to @file_type_id
+	# This is used by the files method
 	def check_file_type
 		if file_type = params['file_type']
 			FileType.pluck(:id, :title).each do |ft|
@@ -103,6 +131,9 @@ class ApiController < ApplicationController
 		end
 	end
 
+	##
+	# Renders the @response hash as jsons
+	# Should be called at the end of every API method
 	def respond
 		render :json => @response.to_json
 	end
