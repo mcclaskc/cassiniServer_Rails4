e0e67193f65607ebe253cbafb69ee867efaabeea
Chris McClaskey <christopher.m.mcclaskey@gmail.com>
2013-05-02 18:05:51 -0600

    added tests for api controller

--- test/controllers/api_controller_test.rb
+++ test/controllers/api_controller_test.rb
@@ -1,7 +1,20 @@
 require 'test_helper'
 
 class ApiControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+	test "should get files" do 
+		get :files
+		assert_response :success
+	end
+
+	test "should get ephem" do 
+		get :ephem
+		assert_response :success
+	end
+
+	test "should get events" do 
+		get :events
+		assert_response :success
+	end
+
+
 end
